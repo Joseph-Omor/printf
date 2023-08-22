62da48386905bc0d2cdfa342ee47cf7eb594788a
AkosuaTakyiwaa <nhyiradenkyi@gmail.com>
2023-08-22 16:48:08 -0700

    update on print_functions

--- print_functions.c
+++ print_functions.c
@@ -50,3 +50,45 @@ int print_double_percent(va_list args)
 	write(1, "%%", 1);
 	return (1);
 }
+
+#include "main.h"
+/**
+ * print_int - a function that print digit
+ *
+ * Return: digits
+ */
+int print_int(va_list args) 
+{
+	int num = va_arg(args, int);
+	int temp = num;
+	int digits = 0;
+
+	if (num < 0) 
+	{
+		write(1, "-", 1) /**Print negativesign for negative numbers */
+		num = -num; /** convert negative number to positive */
+	}
+
+	/** count the number of digits */
+	if (num == 0) 
+	{
+		write(1, "0" 1);
+		return (1);
+	}
+
+	while (temp > 0) 
+	{
+		temps /= 10;
+		digits +
+
+	/* convert digits to characters and point */
+	cahr num_str[digits];
+		for (int i = digits - 1; i >= 0; i-) 
+		{
+			num_str[i] = '0' + (nm % 10);
+			num /= 10;
+	}
+
+	write(1, num_str, digits);
+	return (digits);
+}
